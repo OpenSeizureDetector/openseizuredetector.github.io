196ef88c4287443c3552eed59afff0421698be78
Graham Jones <grahamjones139@gmail.com>
2026-05-05 22:25:20 +0100

    Updated theme to make it clearer to read

---
+++ diff_upload.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#ncftpput -R -f ~/Dropbox/openseizuredetector.ftp /public_html/static/osd_pages _site/* 
+
+#lftp -e "set ssl:verify-certificate no; open ftp.openseizuredetector.org.uk; mirror -R ./_site /public_html/static/osd_pages; quit"
+
+
+
+# 1. Build the site specifically for the OSD web server
+echo "Building site for OSD web server..."
+bundle exec jekyll build --config _config.yml,_config_osd.yml
+
+# 2. Upload only changed files using lftp
+echo "Uploading to web server..."
+lftp -e "set ssl:verify-certificate no; open ftp.openseizuredetector.org.uk; mirror -R --ignore-time --parallel=5 --delete ./_site /public_html/static/osd_pages; quit"
+
+echo "Done!"
---
+++ upload.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ncftpput -R -f ~/Dropbox/openseizuredetector.ftp /public_html/static/osd_pages _site/* 
